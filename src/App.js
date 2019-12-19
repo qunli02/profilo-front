5ffc2a3a62fc48688a2af47e09d325e26070b7f1
Qunli <qunli02@gmail.com>
2019-12-19 17:01:16 -0500

    added all component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Welcome from './welcome.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Welcome/>
       </header>
     </div>
   );
---
+++ src/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function welcome() {
+  return (
+    <div className="welcome">
+
+        <a>1223</a>
+
+    </div>
+  );
+}
+
+export default welcome;
