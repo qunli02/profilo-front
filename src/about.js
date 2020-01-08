315e86be12b89e9703f5ea6b0abed5e794e46d9f
Qunli <qunli02@gmail.com>
2020-01-08 12:07:31 -0500

    picture size

--- src/about.js
+++ src/about.js
@@ -7,7 +7,7 @@ function about() {
         <h1>About me</h1>
         <p></p>
         <div>
-          <img src={require(`./public/me.jpg`)} alt="face" style={{float:"left",width:"600px",height:"420px",}}/>
+          <img src={require(`./public/me.jpg`)} alt="face" style={{width:"600px",height:"420px",}}/>
         </div>
         <div>
         <p>A full stack software engineer looking for opportunities further my learning. </p>
