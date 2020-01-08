8ae9a27eedb768aecb75799e26fefead1309db8d
Qunli <qunli02@gmail.com>
2020-01-07 22:49:01 -0500

    project 2

--- src/projects.js
+++ src/projects.js
@@ -10,7 +10,7 @@ function projects() {
         <a href="https://drive.google.com/file/d/17plY5qQDq6J_5yiNq6m2IPpOIFD1a4ie/view" target="_blank">
           <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="face" />
         </a>
-
+        <h2></h2>
     </div>
   );
 }
