7d197c3d21069a5e3dd0de252881db0547fbdf2e
Qunli <qunli02@gmail.com>
2020-01-08 11:58:50 -0500

    picture

--- src/about.js
+++ src/about.js
@@ -7,7 +7,7 @@ function about() {
         <h1>About me</h1>
         <p></p>
         <div>
-          <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt="face" />
+          <img src={require(`./public/me.jpg`)} alt="face" style={{float:"left",width:"600px",height:"420px",}}/>
         </div>
         <div>
         <p>A full stack software engineer looking for opportunities further my learning. </p>
