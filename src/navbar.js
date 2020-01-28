a945244b64d6bb3e753f3b31921ab23a423c3f1c
Qunli <qunli02@gmail.com>
2020-01-28 18:50:42 -0500

    basic navbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './navbar.css';
 
 function navbar() {
   return (
     <div className="navbar">
     <ul>
-      <li><a href="default.asp">Home</a></li>
-      <li><a href="news.asp">News</a></li>
-      <li><a href="contact.asp">Contact</a></li>
-      <li><a href="about.asp">About</a></li>
+      <li><a href="default.asp">Intro</a></li>
+      <li><a href="news.asp">My Projects</a></li>
+      <li><a href="contact.asp">About</a></li>
+      <li><a href="about.asp">Contact</a></li>
     </ul>
     </div>
   );
