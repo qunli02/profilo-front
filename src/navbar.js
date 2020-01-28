7ebeec2ca1dab07f9435876c4f3494da9f2fa53d
Qunli <qunli02@gmail.com>
2020-01-28 18:47:35 -0500

    list

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,12 @@ import React from 'react';
 function navbar() {
   return (
     <div className="navbar">
-
+    <ul>
+      <li><a href="default.asp">Home</a></li>
+      <li><a href="news.asp">News</a></li>
+      <li><a href="contact.asp">Contact</a></li>
+      <li><a href="about.asp">About</a></li>
+    </ul>
     </div>
   );
 }
