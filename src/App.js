f4e4151acee9ed33022f2807f8c68b10afa0b1ff
Qunli <qunli02@gmail.com>
2020-01-28 18:44:53 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import About from './about.js';
 import Projects from './projects.js';
 import Contact from "./contact.js";
+import NavBar from "./navbar.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <Welcome/>
         <Projects/>
         <About/>
