7170af7838fd5c45470aefda654c8a0db8d26af9
Qunli <qunli02@gmail.com>
2020-01-30 18:09:24 -0500

    scroling

--- src/navbar.js
+++ src/navbar.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './navbar.css';
 
 function navbar() {
+
+  function handleClick(e){
+    e.preventDefault()
+    window.scrollTo(0,48)
+  }
+
+
+
   return (
     <div className="navbar">
     <ul>
-      <li><a href="default.asp">Intro</a></li>
+      <li><a onClick={handleClick}>Intro</a></li>
       <li><a href="news.asp">My Projects</a></li>
       <li><a href="contact.asp">About</a></li>
       <li><a href="about.asp">Contact</a></li>
