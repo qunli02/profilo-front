da6964b3aaf362189bcc9d59b8f480e4c374df79
Qunli <qunli02@gmail.com>
2021-03-10 16:40:32 -0500

    added links to source code

--- src/projects.js
+++ src/projects.js
@@ -6,9 +6,14 @@ function projects() {
     <div className="content" id="projects">
       <h1 id="gds">My Projects</h1>
       <h2>Shadow Hunters</h2>
-      <a href="https://github.com/qunli02/mod5-project-front" target="_blank">
-        Source Code
-      </a>
+      <p>
+        This is a board game called "Shadow Hunter" that I made into a web page.
+        This is built on react as front-end and ruby as back end. The{" "}
+        <a href="https://github.com/qunli02/mod5-project-front" target="_blank">
+          Source Code
+        </a>
+      </p>
+
       <br />
       <a
         href="https://drive.google.com/file/d/17plY5qQDq6J_5yiNq6m2IPpOIFD1a4ie/view"
@@ -20,7 +25,22 @@ function projects() {
           style={{ width: "700px", height: "500px" }}
         />
       </a>
-      <h2></h2>
+      <h2>Stocks Exchange</h2>
+      <p>
+        This is a stock exchange web-page I made which simulates being an
+        investor. It will allow to look up stock and buy and sell stock with
+        fake money to see if you will earn or lose money. The{" "}
+        <a href="https://github.com/qunli02/mod5-project-front" target="_blank">
+          Source Code
+        </a>
+        and the{" "}
+        <a
+          href="https://stock-exchange-qunli.herokuapp.com/login"
+          target="_blank"
+        >
+          Web page
+        </a>
+      </p>
     </div>
   );
 }
